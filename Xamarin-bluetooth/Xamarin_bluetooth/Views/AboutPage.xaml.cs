49c7d74b09df741712b786ae5dcc36b63fc3f101
Nelson Vega <nelson@kleiotechnology.com>
2017-05-03 16:36:21 -0400

    xamarin bluetooth

---
+++ Xamarin-bluetooth/Xamarin_bluetooth/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Xamarin_bluetooth.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
